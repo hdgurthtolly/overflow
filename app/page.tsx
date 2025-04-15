9d6f0d202b11c61a5ffa6fb3fae19d0184c5df17
Harry Daniel Gurth Angeles <harry@tolly.io>
2025-04-15 11:15:02 +0200

    Add assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <h1 className='text-primary-500 text-3xl font-black'>Welcome to NextJS Harry</h1>
+    <>
+      <h1 className='text-primary-500 text-3xl font-black'>Welcome to NextJS Harry</h1>
+      <h1 className='font-space-grotesk text-primary-500 text-3xl font-black'>Welcome to NextJS Harry</h1>
+    </>
   );
 };
 
